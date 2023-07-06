d65dcbccd2ab426bf19bb866094f863a146b606c
Adebisi Joy <greenjoy234@gmail.com>
2023-07-06 16:23:10 +0100

    Array and loop exercise

---
+++ script/11t.js
@@ -0,0 +1,16 @@
+function removeEgg(foods) {
+  const reversedFoods = foods.reverse();
+
+  const result = [];
+  let eggsRemoved = 0;
+
+  for (let i = 0; i < foods.length; i++) {
+     if (foods[i] === 'egg' & eggsRemoved < 2) {
+         eggsRemoved++;
+         continue;
+     }
+     result.push(foods[i]);
+  }
+  return result;
+}
+console.log(removeEgg(['egg', 'apple', 'egg', 'egg', 'ham']));
