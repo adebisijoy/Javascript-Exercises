afb470f00fab53e4544ff800aaa1b9c399783ff3
Adebisi Joy <greenjoy234@gmail.com>
2023-07-17 16:00:36 +0100

    Updated the code in one of DOM exercise.

--- script/9eDOM.js
+++ script/9eDOM.js
@@ -1,8 +1,7 @@
 function inputText() {
-  if (event.key === "Enter") {
     const inputElement = document.querySelector(".js-name-input");
     document.querySelector(
       ".js-name"
     ).innerHTML = `Your name is: ${inputElement.value}`;
-  }
+  
 }
