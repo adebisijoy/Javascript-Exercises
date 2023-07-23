88397c5449bbc8cf1f699038c86f2ee9c877dd26
Adebisi Joy <greenjoy234@gmail.com>
2023-07-23 21:49:06 +0100

    Learning Todo list project personally.

--- script/TODOlistpersonal.js
+++ script/TODOlistpersonal.js
@@ -1,21 +1,29 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = ['wash dishes', 'make dinner'];
 
 renderTodoList();
 
 function renderTodoList() {
-let todoListHTML = '';
+   let todoListHTML = '';
 
-for (let i = 0; i < todoList.length; i ++) {
-   const todo = todoList[i];
-   const html = `<p>${todo}</p>`;
-   todoListHTML += html;
+   for (let i = 0; i < todoList.length; i++) {
+      const todo = todoList[i];
+      const html = `
+      <p>
+      ${todo} 
+      <button onclick= "
+       todoList.splice(${i}, 1);
+       renderTodoList();
+      ">Delete</button>
+      </p>`;
+      todoListHTML += html;
+   }
+   console.log(todoListHTML);
+
+   document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
 }
-console.log(todoListHTML);
 
-document.querySelector('.js-todo-list')
- .innerHTML = todoListHTML;
 
-}
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
 
